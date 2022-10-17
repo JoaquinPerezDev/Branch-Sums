35e399bf9a26c434a4bf9574c9fa967efd511987
LucasPerezDev <LucasPerezDev@gmail.com>
2022-10-16 23:56:58 -0400

    Added second variation including the branch sums array as an argument to the branchSums function

--- branchSums.js
+++ branchSums.js
@@ -1,14 +1,13 @@
-//Write a function that takes in a Binary Tree and returns a list of it's branch sums ordered from leftmost branch sum to rightmost branch sum. 
-//A branch sum is the sum of all values in a Binary Tree branch. A BT branch is a path of nodes in a tree that starts at the root node and ends at any leaf node. 
+//Write a function that takes in a Binary Tree and returns a list of it's branch sums ordered from leftmost branch sum to rightmost branch sum.
+//A branch sum is the sum of all values in a Binary Tree branch. A BT branch is a path of nodes in a tree that starts at the root node and ends at any leaf node.
 //Each *BinaryTree* node has an integer *value*, a *left* child node, and a *right* child node. Children nodes can either be BinaryTree nodes themselves or *None*/*null*.
-
 
 // Sample input: tree =    1
 //                       /   \
 //                      2     3
-//                   /   \  /  \  
+//                   /   \  /  \
 //                  4    5 6   7
-//                /  \  /  
+//                /  \  /
 //               8   9 10
 
 // Sample Output: [15, 16, 18, 10, 11]
@@ -31,18 +30,39 @@ class BinaryTree {
 
 function branchSums(root, res = 0) {
   // Write your code here.
-  if(!root) {
+  if (!root) {
     return [];
   }
   let currentSum = res + root.value;
-  if(!root.left && !root.right) {
+  if (!root.left && !root.right) {
     return [currentSum];
   }
 
   let allSums = [
     ...branchSums(root.left, currentSum),
-    ...branchSums(root.right, currentSum)
+    ...branchSums(root.right, currentSum),
   ];
-  
+
   return allSums;
 }
+
+//Below is a variation on the above code showecasing more destructuring. You can pass in the branch sums array into the branchSums function. This allows us to push the currentSum to the end of the sums array at every instance in which we reach a leaf node.
+
+// class BinaryTree {
+//   constructor(value) {
+//     this.value = value;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+// function branchSums(root, sum = 0, sums = []) {
+
+//   const currentSum = root.value + sum;
+
+//   if(!root.left && !root.right) sums.push(currentSum);
+//   root.left && branchSums(root.left, currentSum, sums);
+//   root.right && branchSums(root.right, currentSum, sums);
+
+//   return sums;
+// }
